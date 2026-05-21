b9ea85ad090102549da22a947f1b32c274887e49
Jeeten Mandal <jeeten.cloveit@gmail.com>
2026-05-21 11:54:07 +0545

    hero changes

---
+++ src/components/Hero.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='grid grid-cols-2 gap-4'>
+        <div>
+            <h1>Chat with AI built for developers</h1>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore soluta corporis, voluptates nisi temporibus nam molestiae quo quisquam sint saepe eum quae minus provident nemo porro fuga exercitationem, optio ad!</p>
+            <button>Get Started</button>
+        </div>
+
+        <div>
+            <img src="https://media.gadgetbytenepal.com/2025/01/IT-Company.jpg" alt="ISMT chat office" />
+        </div>
+    </div>
+  )
+}
+
+export default Hero
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import Hero from '../components/Hero'
 
 const Landing = () => {
   return (
     <div>
         <Navbar />
+        <Hero />
     </div>
 
   )
